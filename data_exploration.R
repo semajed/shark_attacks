936b41dc8d18bbcaebc9d7d58b3c22898b4f7b5c
James <dayhuffj@gmail.com>
2016-10-12 18:37:59 -0600

    narrowed down highest risk area for shark attacks

--- data_exploration.R
+++ data_exploration.R
@@ -8,9 +8,27 @@ str(sharks)
 
 head(sharks)
 
-head(sharks$Type)
+summary(sharks)
 
 shark_country = as.factor(sharks$Country) 
 attack_type = as.factor(sharks$Type)
-barplot(prop.table(table(attack_type)))
-hist(sharks$age)
+barplot(prop.table(table(shark_country)))
+
+### TOP 3 COUNTRIES WITH HIGHEST RECORDS ###
+barplot(sort(table(shark_country),decreasing=TRUE)[1:3])
+
+
+### FOCUSING IN ON THE USA AREAS ###
+sharks.sub = subset(sharks, sharks$Country == "USA")
+View(sharks.sub)
+
+shark_areas = as.factor(sharks.sub$Area)
+barplot(prop.table(table(shark_areas)))
+### FLORIDA IS THE MOST DANGEROUS. OR THEY HAVE THE BEST RECORDING HABITS
+barplot(sort(table(shark_areas),decreasing=TRUE)[1:3])
+
+
+areas.sub = subset(sharks.sub, sharks.sub$Area == "Florida")
+shark_location = as.factor(areas.sub$Location)
+barplot(sort(table(shark_location),decreasing=TRUE)[1:3])
+
